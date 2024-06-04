7bbd42a7212d6dd82d7e53a0f20b71a44600471d
Adriano Valadar <adriano.valadar@outlook.com>
2024-06-05 00:05:32 +0100

    add tests for date: test daysInMonth

--- tests/DateTests.cpp
+++ tests/DateTests.cpp
@@ -11,12 +11,42 @@ date setDate(const int d, const int m, const int y) {
     return date;
 }
 
-TEST_F(DateTests, isLeapYearExpectTrue) {
+TEST(isLeapYear, ExpectTrue) {
     constexpr int year = 2024;
     EXPECT_EQ(isLeapYear(year), true);
 }
 
-TEST_F(DateTests, isLeapYearExpectFalse) {
+TEST(isLeapYear, ExpectFalse) {
     constexpr int year = 2023;
     EXPECT_EQ(isLeapYear(year), false);
+}
+
+TEST(DaysInMonthTest, DaysInMonthCommonYear) {
+    EXPECT_EQ(daysInMonth(1, 2023), 31);
+    EXPECT_EQ(daysInMonth(2, 2023), 28);
+    EXPECT_EQ(daysInMonth(3, 2023), 31);
+    EXPECT_EQ(daysInMonth(4, 2023), 30);
+    EXPECT_EQ(daysInMonth(5, 2023), 31);
+    EXPECT_EQ(daysInMonth(6, 2023), 30);
+    EXPECT_EQ(daysInMonth(7, 2023), 31);
+    EXPECT_EQ(daysInMonth(8, 2023), 31);
+    EXPECT_EQ(daysInMonth(9, 2023), 30);
+    EXPECT_EQ(daysInMonth(10, 2023), 31);
+    EXPECT_EQ(daysInMonth(11, 2023), 30);
+    EXPECT_EQ(daysInMonth(12, 2023), 31);
+}
+
+TEST(DaysInMonthTest, LeapYear) {
+    EXPECT_EQ(daysInMonth(1, 2024), 31);
+    EXPECT_EQ(daysInMonth(2, 2024), 29);
+    EXPECT_EQ(daysInMonth(3, 2024), 31);
+    EXPECT_EQ(daysInMonth(4, 2024), 30);
+    EXPECT_EQ(daysInMonth(5, 2024), 31);
+    EXPECT_EQ(daysInMonth(6, 2024), 30);
+    EXPECT_EQ(daysInMonth(7, 2024), 31);
+    EXPECT_EQ(daysInMonth(8, 2024), 31);
+    EXPECT_EQ(daysInMonth(9, 2024), 30);
+    EXPECT_EQ(daysInMonth(10, 2024), 31);
+    EXPECT_EQ(daysInMonth(11, 2024), 30);
+    EXPECT_EQ(daysInMonth(12, 2024), 31);
 }
