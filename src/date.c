5edc2a1eeeecea3ca8942f89bc8c691e92a631b5
Adriano Valadar <adriano.valadar@outlook.com>
2024-06-10 01:13:01 +0100

    document date: add doxygen comments
    update signatures of functions

--- src/date.c
+++ src/date.c
@@ -26,45 +26,45 @@ bool isEmptyDate(const date d) {
     return d.day == 0 && d.month == 0 && d.year == 0;
 }
 
-int diffInDays(const date date1, const date date2) {
-    int daysDate1 = date1.day;
-    int daysDate2 = date2.day;
-    for (int m = 1; m < date1.month; m++) {
-        daysDate1 += daysInMonth(m, date1.year);
+int diffInDays(const date d1, const date d2) {
+    int daysDate1 = d1.day;
+    int daysDate2 = d2.day;
+    for (int m = 1; m < d1.month; m++) {
+        daysDate1 += daysInMonth(m, d1.year);
     }
-    for (int m = 1; m < date2.month; m++) {
-        daysDate2 += daysInMonth(m, date2.year);
+    for (int m = 1; m < d2.month; m++) {
+        daysDate2 += daysInMonth(m, d2.year);
     }
-    for (int y = START_YEAR; y < date1.year; y++) {
+    for (int y = START_YEAR; y < d1.year; y++) {
         daysDate1 += 365 + (int)isLeapYear(y);
     }
-    for (int y = START_YEAR; y < date2.year; y++) {
+    for (int y = START_YEAR; y < d2.year; y++) {
         daysDate2 += 365 + (int)isLeapYear(y);
     }
     return abs(daysDate2 - daysDate1);
 }
 
-bool areDatesEqual(const date date1, const date date2) {
-    return date1.day == date2.day && date1.month == date2.month && date1.year == date2.year;
+bool areDatesEqual(const date d1, const date d2) {
+    return d1.day == d2.day && d1.month == d2.month && d1.year == d2.year;
 }
 
-bool isDateWithinRange(const date start, const date end, const date input) {
-    if (input.year < start.year || input.year > end.year) {
+bool isDateWithinRange(const date start, const date end, const date d) {
+    if (d.year < start.year || d.year > end.year) {
         return 0;
     }
-    if (input.year == start.year && (input.month < start.month || (input.month == start.month && input.day < start.day))) {
+    if (d.year == start.year && (d.month < start.month || (d.month == start.month && d.day < start.day))) {
         return 0;
     }
-    if (input.year == end.year && (input.month > end.month || (input.month == end.month && input.day > end.day))) {
+    if (d.year == end.year && (d.month > end.month || (d.month == end.month && d.day > end.day))) {
         return 0;
     }
     return 1;
 }
 
-bool isDateBefore(const date date1, const date date2) {
-    return (date1.day < date2.day && date1.month == date2.month && date1.year == date2.year) || (date1.month < date2.month && date1.year == date2.year) || date1.year < date2.year;
+bool isDateBefore(const date d1, const date d2) {
+    return (d1.day < d2.day && d1.month == d2.month && d1.year == d2.year) || (d1.month < d2.month && d1.year == d2.year) || d1.year < d2.year;
 }
 
-bool isDateAfter(const date date1, const date date2) {
-    return date1.year > date2.year || (date1.year == date2.year && date1.month > date2.month) || (date1.year == date2.year && date1.month == date2.month && date1.day > date2.day);
+bool isDateAfter(const date d1, const date d2) {
+    return d1.year > d2.year || (d1.year == d2.year && d1.month > d2.month) || (d1.year == d2.year && d1.month == d2.month && d1.day > d2.day);
 }
--- src/date.h
+++ src/date.h
@@ -3,19 +3,81 @@
 
 #include <stdbool.h>
 
+/**
+ * @brief Structure representing a date.
+ */
 typedef struct date {
-    int day;
-    int month;
-    int year;
+    int day;   /**< Day of the month */
+    int month; /**< Month of the year */
+    int year;  /**< Year */
 } date;
 
+/**
+ * @brief Determines if a given year is a leap year.
+ *
+ * @param year The year to check.
+ * @return `true` if the year is a leap year, `false` otherwise.
+ */
 bool isLeapYear(int year);
+/**
+ * @brief Checks if a date is empty.
+ *
+ * An empty date is defined as having all fields set to 0.
+ *
+ * @param d date to check.
+ * @return `true` if the date is empty, `false` otherwise.
+ */
 bool isEmptyDate(date d);
-int diffInDays(date date1, date date2);
+/**
+ * @brief Calculates the difference in days between dates.
+ *
+ * @param d1 date for comparison.
+ * @param d2 date for comparison.
+ * @return The number of days between the two dates.
+ */
+int diffInDays(date d1, date d2);
+/**
+ * @brief Retrieves the number of days in a given month
+ *
+ * @param month Month to check.
+ * @param year Year to check.
+ * @return The number of days in the specified month.
+ */
 int daysInMonth(int month, int year);
-bool areDatesEqual(date date1, date date2);
-bool isDateWithinRange(date start, date end, date input);
-bool isDateBefore(date date1, date date2);
-bool isDateAfter(date date1, date date2);
+/**
+ * @brief Checks if two dates are equal.
+ *
+ * @param d1 Date for comparison.
+ * @param d2 Date for comparison.
+ * @return `true` if the dates are equal, `false` otherwise.
+ */
+bool areDatesEqual(date d1, date d2);
+/**
+ * @brief Checks if a given date is in a range.
+ *
+ * The range includes the start and end dates.
+ *
+ * @param start Beginning of the range.
+ * @param end End of the range.
+ * @param d Date to check.
+ * @return `true` if the date is within the range, `false` otherwise.
+ */
+bool isDateWithinRange(date start, date end, date d);
+/**
+ * @brief Checks if a date is before other date.
+ *
+ * @param d1 Reference date.
+ * @param d2 Date to be compared with.
+ * @return `true` if d1 is before d2, `false` otherwise.
+ */
+bool isDateBefore(date d1, date d2);
+/**
+ * @brief Checks if a date is after other date.
+ *
+ * @param d1 Reference date.
+ * @param d2 Date to be compared with.
+ * @return `true` if d1 is after d2, `false` otherwise.
+ */
+bool isDateAfter(date d1, date d2);
 
 #endif//RENT_A_CAR_DATE_H
