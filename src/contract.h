6a044a6e99a64b90613a0c1a5edc66cbf11232f1
Adriano Valadar <adriano.valadar@outlook.com>
2024-07-12 23:28:43 +0100

    document contract: add doxygen comments

--- src/contract.h
+++ src/contract.h
@@ -5,22 +5,71 @@
 #include "date.h"
 #include "vehicle.h"
 
+/**
+ * @brief Structure to store information about a rental contract.
+ */
 typedef struct contract {
-    int codeVehicle;
-    int codeCustomer;
-    float priceDay;
-    float quantityKm;
-    date startDate;
-    date endDate;
-    int startOffice;
-    int endOffice;
-    float price;
+    int codeVehicle;    /**< Unique code identifying the vehicle */
+    int codeCustomer;   /**< Unique code identifying the customer */
+    float priceDay;     /**< Price per day of the rental */
+    float quantityKm;   /**< Quantity of kilometers the vehicle did during the rental */
+    date startDate;     /**< Start date of the rental */
+    date endDate;       /**< End date of the rental */
+    int startOffice;    /**< Code of the office where the rental starts */
+    int endOffice;      /**< Code of the office where the rental ends */
+    float price;        /**< Total price of the rental */
 } contract;
 
+/**
+ * @brief Displays the list of rental contracts.
+ *
+ * @param contracts Array of contracts to be displayed.
+ * @param quantity Number of contracts in the array.
+ */
 void showContracts(const contract *contracts, size_t quantity);
+
+/**
+ * @brief Starts a new rental contract.
+ *
+ * @param file Pointer to the file to read from.
+ * @param contracts Array of contracts.
+ * @param customers Array of customers.
+ * @param vehicles Array of vehicles.
+ * @param quantityContracts Number of contracts.
+ * @param quantityCustomers Number of customers in the array.
+ * @param quantityVehicles Number of vehicles in the array.
+ */
 void startContract(FILE *file, contract *contracts, customer *customers, vehicle *vehicles, size_t *quantityContracts, size_t quantityCustomers, size_t quantityVehicles);
+
+/**
+ * @brief Manages an existing contract by vehicle code and start date.
+ *
+ * @param file Pointer to the file to read from.
+ * @param contracts Array of contracts.
+ * @param vehicles Array of vehicles.
+ * @param customers Array of customers.
+ * @param quantityContracts Number of contracts.
+ * @param quantityVehicles Number of vehicles in the array.
+ * @param quantityCustomers Number of customers in the array.
+ */
 void manageContractByVehicleCodeAndStartDate(FILE *file, contract *contracts, vehicle *vehicles, customer *customers, size_t *quantityContracts, size_t quantityVehicles, size_t quantityCustomers);
+
+/**
+ * @brief Reads the contracts from a file.
+ *
+ * @param fileName Name of the file to read contracts from.
+ * @param contracts Array to store the read contracts.
+ * @param quantity Pointer to store the number of contracts read.
+ */
 void readContracts(char *fileName, contract *contracts, size_t *quantity);
+
+/**
+ * @brief Writes the contracts to a file.
+ *
+ * @param fileName Name of the file to write contracts to.
+ * @param contracts Array of contracts to be written.
+ * @param quantity Number of contracts in the array.
+ */
 void writeContracts(char *fileName, const contract *contracts, size_t quantity);
 
 #endif//RENT_A_CAR_CONTRACT_H
