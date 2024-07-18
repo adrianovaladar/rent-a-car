48464f65a8d5964f86f24e5e3b5859a42efd4233
Adriano Valadar <adriano.valadar@outlook.com>
2024-07-18 22:02:17 +0100

    document application: add doxygen comments

--- src/application.h
+++ src/application.h
@@ -1,6 +1,13 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
+/**
+ * @brief Runs the Rent-a-Car Management System.
+ *
+ * This function initializes the application, displays the main menu,
+ * processes user input and manages customers, vehicles, and contracts.
+ * It repeatedly displays the menu until the user chooses to exit.
+ */
 void run();
 
 #endif //APPLICATION_H
