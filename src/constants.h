d603b143fb5e1a716e1193d2948fa34e6fc8b6f7
Adriano Valadar <adriano.valadar@outlook.com>
2025-04-28 01:31:44 +0100

    document constants

--- src/constants.h
+++ src/constants.h
@@ -1,12 +1,40 @@
 #ifndef RENT_A_CAR_CONSTANTS_H
 #define RENT_A_CAR_CONSTANTS_H
 
+/**
+ * @def MAX_CUSTOMERS
+ * @brief Maximum number of customers the system can handle.
+ */
 #define MAX_CUSTOMERS 1000
+/**
+ * @def MAX_VEHICLES
+ * @brief Maximum number of vehicles managed by the system.
+ */
 #define MAX_VEHICLES 300
+/**
+ * @def MAX_OFFICES
+ * @brief Maximum number of rental offices.
+ */
 #define MAX_OFFICES 7
+/**
+ * @def MAX_CONTRACTS
+ * @brief Maximum number of rental contracts the system can track.
+ */
 #define MAX_CONTRACTS 1000
+/**
+ * @def START_YEAR
+ * @brief The starting year for rental operations.
+ */
 #define START_YEAR 2020
+/**
+ * @def EPSILON
+ * @brief A small value used for floating-point comparisons.
+ */
 #define EPSILON 1e-6
+/**
+ * @def LENGTH_LOG
+ * @brief Maximum length of a single log entry or the log buffer.
+ */
 #define LENGTH_LOG 500
 
 #endif//RENT_A_CAR_CONSTANTS_H
